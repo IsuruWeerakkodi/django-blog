3e1351e10823fb82ea64fd0d5f28149bac070489
Isuru Weerakkodi <isuruweerakkodi24@gmail.com>
2024-02-07 23:44:30 +0530

    complete admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,40 @@
 from django.contrib import admin
-from blog.models import School, ClassName
+from blog.models import School, ClassName, AssessmentAreas, Student, Answers, Awards, Subject
+
+
 # Register your models here.
 class SchoolAdmin(admin.ModelAdmin):
     pass
 
+
 class ClassNameAdmin(admin.ModelAdmin):
     pass
 
+
+class AssessmentAreasAdmin:
+    pass
+
+
+class StudentAdmin:
+    pass
+
+
+class AnswersAdmin:
+    pass
+
+
+class AwardsAdmin:
+    pass
+
+
+class SubjectAdmin:
+    pass
+
+
 admin.site.register(School, SchoolAdmin)
 admin.site.register(ClassName, ClassNameAdmin)
+admin.site.register(AssessmentAreas, AssessmentAreasAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Answers, AnswersAdmin)
+admin.site.register(Awards, AwardsAdmin)
+admin.site.register(Subject, SubjectAdmin)
--- blog/models.py
+++ blog/models.py
@@ -14,15 +14,38 @@ class ClassName(models.Model):
     def __str__(self) -> str:
         return f"{self.class_name}"
 
+class AssessmentAreas(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return f"{self.name}"
 
-#create models
-#flush database
-#make migrations
-#migrate
-#complete admin.py
-#create super user
-#populate databse script run
-#schools.html  -> create other htmls
-#COMPLETE URL.PY WITH OTHER HTML (BLOG/URL.PY)
-#complete views
+class Student(models.Model):
+    id = models.AutoField(primary_key=True)
+    full_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.full_name}"
+
+class Answers(models.Model):
+    id = models.AutoField(primary_key=True)
+    answers = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.answers}"
+
+class Awards(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+class Subject(models.Model):
+    id = models.AutoField(primary_key=True)
+    subject = models.CharField(max_length=255)
+    subject_score = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.subject, self.subject_score}"
